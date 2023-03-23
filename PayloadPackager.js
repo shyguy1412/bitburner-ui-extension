7a1778ebb5d9dc20eb5821e66f57935e051de250
Nils Ramstoeck <nilsramstoeck@gmail.com>
2023-03-23 19:45:15 +0100

    fixed missing folder

--- PayloadPackager.js
+++ PayloadPackager.js
@@ -6,20 +6,23 @@ const filename = 'ui';
 const jsfile = path.join(__dirname, 'dist', `${filename}.js`);
 const cssFile = path.join(__dirname, 'dist', `${filename}.css`);
 
-if(process.argv[2] == 'w'){
+if (process.argv[2] == 'w') {
   fs.watchFile(jsfile, () => package());
   fs.watchFile(cssFile, () => package());
 }
 
 function package() {
+  if (!fs.existsSync(path.join(__dirname, 'src/bitburner/dist')))
+    fs.mkdirSync(path.join(__dirname, 'src/bitburner/dist'));
   fs.writeFileSync(
     path.join(__dirname, 'src/bitburner/dist', 'payload.js'),
-   /*js*/`export const payload=['${encode(jsfile)}', '${encode(cssFile)}']`
+   /*js*/`export const payload=['${encode(jsfile)}', '${encode(cssFile)}']`,
+    {}
   )
   console.log(new Date() + 'Packaged');
 }
 
-function encode(string){
+function encode(string) {
   return Buffer.from(fs.readFileSync(string)).toString('base64')
 }
 
